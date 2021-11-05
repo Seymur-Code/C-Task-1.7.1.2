940497e0011f339ebdd0efcfa653d899a5db79fa
Seymur Abbasov <seymur.abbasov.q@student.aztu.edu.az>
2021-11-05 13:45:48 +0400

    Add project files.

---
+++ C-Task-1.7.1.2/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Task_1._7._1._2
+{
+    class Product
+    {
+        #region Task-1
+        public string Name;
+        public int Price;
+        public int Count;
+        public int TotalIncome;
+        #endregion
+
+
+
+        #region Task-1(method)
+        public void Sell()
+        {
+
+
+
+        }
+
+        #endregion
+
+    }
+
+
+
+
+
+
+}
---
+++ C-Task-1.7.1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_Task_1._7._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product pr1 = new Product();
+
+            pr1.Name = "Paysut";
+            pr1.Price = 5;
+            pr1.Count = 20;
+            pr1.TotalIncome= 5000;
+
+        }
+    }
+}
